93ec32a0c0f1775af39f1bbfaaecad5481a177a6
Elena Pavlova <lenlho22@gmail.com>
2018-02-11 20:40:43 +0300

    Correct gulp for minification styles

--- gulpfile.js
+++ gulpfile.js
@@ -89,7 +89,6 @@ gulp.task("style", function() {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("build/css"))
     .pipe(minify())
     .pipe(rename("style.min.css"))
     .pipe(gulp.dest("build/css"))
