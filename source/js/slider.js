3e575e81954f11aae5d45c09f9f9222e31c210d8
Elena Pavlova <lenlho22@gmail.com>
2018-02-14 12:55:49 +0300

    Add gray after background for slider

--- source/js/slider.js
+++ source/js/slider.js
@@ -3,11 +3,17 @@
 
   var separator = document.querySelector('.slider__bar > span');
   var sliderBar = document.querySelector('.slider__bar');
-  var imgBefore = document.querySelector('.slider__img--before');
-  var imgAfter = document.querySelector('.slider__img--after');
+  // var imgBefore = document.querySelector('.slider__img--before');
+  // var imgAfter = document.querySelector('.slider__img--after');
   var imgRight = document.querySelector('.slider__img--right');
   var imgLeft = document.querySelector('.slider__img--left');
+  var controlBefore = document.querySelector('.slider__control--before');
+  var controlAfter = document.querySelector('.slider__control--after');
+  // var sliderFon = document.querySelector('.slider__fon');
   var flag = false;
+  var SEPARATOR_BEFORE = 3;
+  var SEPARATOR_AFTER = 43;
+  var TABLET_WIDTH = 768;
 
   function changeeSlider(proc) {
   //     imgBefore.style.width = proc + '%';
@@ -21,14 +27,10 @@
   separator.addEventListener('mousedown', function(event) {
     event.preventDefault();
     flag = true;
-
-    console.log('mousedown');
   }, false);
 
   document.addEventListener('mouseup', function(event) {
     flag = false;
-
-    console.log('mouseup');
     separator.style.borderColor = 'green';
   }, false);
 
@@ -37,6 +39,7 @@
 
     if (flag && (res > 0) && (res < sliderBar.offsetWidth)) {
       separator.style.left = res + 'px';
+
       var proc = res/sliderBar.offsetWidth*100;
       if (proc > 97) {
         proc = 100;
@@ -44,8 +47,6 @@
       if (proc < 3) {
         proc = 0;
       }
-      // console.log('res ' + res);
-      // console.log('proc = ' + proc);
 
       separator.style.borderColor = 'red';
       changeeSlider(proc);
@@ -58,19 +59,43 @@
 
   function initSlider() {
     var proc;
-    if (window.innerWidth < 768) {
-      // var proc = parseInt(imgBefore.style.width, 10);
+    if (window.innerWidth < TABLET_WIDTH) {
       proc = parseInt(imgLeft.style.width, 10);
-      var newRes = proc < 50 ? 3 : 43;
+      var newRes = proc < 50 ? SEPARATOR_BEFORE : SEPARATOR_AFTER;
       proc = proc < 50 ? 0 : 100;
       separator.style.left = newRes + 'px';
       changeeSlider(proc);
     } else {
-      // var proc = imgBefore.style.width;
       proc = imgLeft.style.width;
       separator.style.left = proc;
       changeeSlider(proc);
     }
+  }
+
+  var beforeSlide = function() {
+    if (window.innerWidth < TABLET_WIDTH) {
+      separator.style.left = SEPARATOR_BEFORE + 'px';
+    } else {
+      separator.style.left = '0%';
+    }
+    changeeSlider(0);
+  };
+
+  var afterSlide = function() {
+    if (window.innerWidth < TABLET_WIDTH) {
+      separator.style.left = SEPARATOR_AFTER + 'px';
+    } else {
+      separator.style.left = '100%';
+    }
+
+    changeeSlider(100);
+  };
+
+  controlBefore.addEventListener('click', beforeSlide);
+  controlAfter.addEventListener('click', afterSlide);
+
+  if (window.innerWidth < TABLET_WIDTH) {
+    controlBefore.click();
   }
 
 })();
